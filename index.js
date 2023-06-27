8e7135c2085286d8cb2f6ac37101af2b4d118736
Art3Aga <alberyarte@gmail.com>
2023-06-27 16:54:01 -0600

    se elimina funcion para obtener datos de la alarma

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ const port = new SerialPort({
 
 const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
-obtenerDatosdeAlarma();
+// obtenerDatosdeAlarma();
 
 // Escucha los datos recibidos desde Arduino
 parser.on("data", (data) => {
   console.log(data);
-  // const datosParseados = parsearDatos(data);
-  // guardarDatosFirestore(datosParseados);
+  const datosParseados = parsearDatos(data);
+  guardarDatosFirestore(datosParseados);
 });
 
 // parser.write()
@@ -35,19 +35,20 @@ function parsearDatos(data) {
   const humedad = datos[0];
   const temperatura = datos[1];
   const lluvia = datos[2];
-  const co2 = datos[3];
+  const co = datos[3];
   const luz = datos[4];
   const presion = datos[5];
+  const viento = datos[6];
 
   const datosParseados = {
     fecha: admin.firestore.Timestamp.now(),
     humedad,
     temperatura,
     lluvia,
-    co2,
+    co,
     luz,
     presion,
-    viento: (Math.random() * 200).toFixed(2),
+    viento,
   };
 
   return datosParseados;
@@ -56,25 +57,22 @@ function parsearDatos(data) {
 // Función para guardar los datos en Firestore
 function guardarDatosFirestore(datos) {
   const id = new Date().valueOf().toString();
-  db.collection("sensor_data").doc(id).set(datos).then((docRef) => {
-    // console.log('Datos guardados en Firestore con ID:', docRef.id);
-  })
-  .catch((error) => {
+  db.collection("sensor_data").doc(id).set(datos).then().catch((error) => {
     console.error("Error al guardar los datos en Firestore:", error);
   });
 }
 
 
-function obtenerDatosdeAlarma() {
-  db.collection('alarma').onSnapshot(snapshot => {
-    // const data = {
-    //   co2: snapshot.docs[0]?.data().valor,
-    //   temp: snapshot.docs[2]?.data().valor
-    // }
+// function obtenerDatosdeAlarma() {
+//   db.collection('alarma').onSnapshot(snapshot => {
+//     // const data = {
+//     //   co2: snapshot.docs[0]?.data().valor,
+//     //   temp: snapshot.docs[2]?.data().valor
+//     // }
 
-    const data = [snapshot.docs[2]?.data().valor,snapshot.docs[0]?.data().valor]
-    // parser.write(JSON.stringify(data.toString()));
-    parser.write(data.toString());
+//     const data = [snapshot.docs[2]?.data().valor,snapshot.docs[0]?.data().valor]
+//     // parser.write(JSON.stringify(data.toString()));
+//     parser.write(data.toString());
 
-  });
-}
+//   });
+// }
